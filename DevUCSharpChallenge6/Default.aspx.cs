3265a6fe58d79a1ddbef7793757b7f53fa71119d
Burris, Wayne <Wayne.Burris@protective.com>
2017-08-29 15:01:24 -0500

    added code to clear out input boxes and only display 2 decimal points on average

--- DevUCSharpChallenge6/Default.aspx.cs
+++ DevUCSharpChallenge6/Default.aspx.cs
@@ -32,12 +32,12 @@ namespace DevUCSharpChallenge6
             int newLength = electionsRigged.Length + 1;
             Array.Resize(ref electionsRigged, newLength);
             int newIndex = electionsRigged.GetUpperBound(0);
-            electionsRigged[newIndex] = int.Parse(electionsRiggedTextBox.Text);
+            electionsRigged[newIndex] = double.Parse(electionsRiggedTextBox.Text);
             ViewState["ElectionsRigged"] = electionsRigged;
 
             double[] actsOfSubterfuge = (double[])ViewState["ActsOfSubterfuge"];
             Array.Resize(ref actsOfSubterfuge, newLength);
-            actsOfSubterfuge[newIndex] = int.Parse(actsOfSubterfugeTextBox.Text);
+            actsOfSubterfuge[newIndex] = double.Parse(actsOfSubterfugeTextBox.Text);
             ViewState["ActsOfSubterfuge"] = actsOfSubterfuge;
 
             string[] assetNames = (string[])ViewState["AssetNames"];
@@ -46,8 +46,13 @@ namespace DevUCSharpChallenge6
             ViewState["AssetNames"] = assetNames;
 
             resultLabel.Text = String.Format("Total Elections Rigged: {0}<br ?>" +
-                "Average Acts of Subterfuge per Asset: {1}<br />" +
+                "Average Acts of Subterfuge per Asset: {1:N2}<br />" +
                 "(Last Asset you added: {2})", electionsRigged.Sum(), actsOfSubterfuge.Average(), assetNames[newIndex]);
+
+            electionsRiggedTextBox.Text = "";
+            actsOfSubterfugeTextBox.Text = "";
+            assetNameTextBox.Text = "";
+
         }
     }
 }
